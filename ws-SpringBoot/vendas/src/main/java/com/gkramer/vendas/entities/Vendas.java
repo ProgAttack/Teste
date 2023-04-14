f0c3cf257a7c22b10386fcb85c2227017542cf56
ProgAttack <92767138+ProgAttack@users.noreply.github.com>
2023-04-14 19:07:30 -0300

    Criando projeto
    Implementar o modelo de domínio
    Mapeamento objeto-relacional com JPA
    Configurar o banco de dados H2

---
+++ ws-SpringBoot/vendas/src/main/java/com/gkramer/vendas/entities/Vendas.java
@@ -0,0 +1,63 @@
+package com.gkramer.vendas.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_vendas")
+public class Vendas {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String data;
+	private double valor;
+	
+	@ManyToOne
+	@JoinColumn(name = "vendedor_id" )
+	private Vendedor vendedor;
+	
+
+	public Vendas() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	
+	
+}
